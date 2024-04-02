14bee42f74880e24ea7844e39b3c7c96b1a6fdc8
shouryatyagi222 <shouryatyagi222@gmail.com>
2024-04-02 12:08:19 +0530

    Update the model

--- model/model.py
+++ model/model.py
@@ -6,7 +6,7 @@ warnings.filterwarnings("ignore")
 import torch.nn.functional as F
 import random
 
-from ..config import *
+from config import *
 
 # config = Config()
 
@@ -43,41 +43,35 @@ class MultiHeadSelfAttention(nn.Module):
         attention_output = torch.matmul(attention_weights, value)
 
         attention_output = attention_output.permute(0, 2, 1, 3).contiguous().view(batch_size, seq_len, embed_dim)
-        # print(attention_output.shape)
-        output = self.fc_out(attention_output)  # Adjusted output
+        output = self.fc_out(attention_output)
 
         return output, attention_output
 
 class Generator(nn.Module):
-    def __init__(self, max_len, vocab_size, pretrained_model, additional_layers, feature_extractor, device):
+    def __init__(self, max_len, vocab_size, pretrained_model, feature_extractor, device):
         super(Generator, self).__init__()
         self.max_len = max_len
         self.vit = pretrained_model
+        self.vit.eval()
         self.global_max_pool = nn.AdaptiveMaxPool1d(1)
         self.flatten = nn.Flatten()
         self.feature_extractor = feature_extractor
-        self.additional_layers = additional_layers
+        self.additional_layers = torch.nn.Sequential(
+                                    torch.nn.Linear(197, 64),
+                                    torch.nn.ReLU(),
+                                    torch.nn.Linear(64, 256)
+                                )
 
         self.embedding = nn.Embedding(vocab_size, 256)
-        self.lstm = nn.LSTM(256, 512, 4, batch_first=True, dropout=0.2)  # Increase LSTM hidden size and add dropout
-        self.fc1 = nn.Linear(512, 256)  # Add additional fully connected layer
-        self.fc2 = nn.Linear(256, 128)  # Add another fully connected layer
-        self.relu = nn.ReLU()  # Add ReLU activation function
-        self.dropout = nn.Dropout(0.5)  # Add dropout layer
+        self.lstm = nn.LSTM(256, 512, 4, batch_first=True, dropout=0.2)
+        self.fc1 = nn.Linear(512, 256)
+        self.fc2 = nn.Linear(256, 256)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.5)
         self.device = device
 
-        # Move modules to the specified device
-        self.embedding.to(device)
-        self.lstm.to(device)
-        self.fc1.to(device)
-        self.fc2.to(device)
-        self.relu.to(device)
-        self.dropout.to(device)
-
         self.fc_combined = MultiHeadSelfAttention(embed_dim=2*128, num_heads=8, vocab_size=vocab_size).to(device)
-        # Increase the number of attention heads to 8 for increased complexity
-        # Define diversity temperature
-        self.diversity_temperature = 1.0  # Adjust this value to control the diversity level
+        self.diversity_temperature = 1.0
 
     def forward(self, image_input, targets=None):
         inputs = self.feature_extractor(images=image_input, return_tensors="pt").to(self.device)
@@ -88,59 +82,94 @@ class Generator(nn.Module):
         flattened = self.flatten(x_pooled)
         final_img_features = self.additional_layers(flattened)
 
-        # sequences = torch.tensor([[1] + [0 for _ in range(self.max_len - 1)] for _ in range(final_img_features.shape[0])], dtype=torch.long).to(self.device)
         sequences = torch.tensor([[1] for _ in range(final_img_features.shape[0])], dtype=torch.long).to(self.device)  # Start token
 
-        yhat_index, hidden_states = self.get_word(sequences, final_img_features, 0)
-        sequences = torch.cat((sequences, yhat_index.T), dim=1)
+        # yhat_index, hidden_states = self.get_word(sequences, final_img_features, 0)
 
-        # # Teacher forcing
-        # if targets is not None and random.random() < TEACHER_FORCING_RATIO:
-        #     for i in range(self.max_len - 1):
-        #         yhat, hidden_states = self.fc_combined(hidden_states)
-        #         yhat_probs = F.softmax(yhat, dim=2)
-        #         _, yhat_index = torch.max(yhat_probs, dim=2)
-        #         sequences = torch.cat((sequences, yhat_index.T), dim=1)
-        # else:
-        for i in range(self.max_len - 2):
-            yhat, hidden_states = self.fc_combined(hidden_states)
-            yhat_probs = F.softmax(yhat, dim=2)
-            _, yhat_index = torch.max(yhat_probs, dim=2)
-            sequences = torch.cat((sequences, yhat_index.T), dim=1)
-            # print(sequences)
-
-        return sequences
-    
-    def get_word(self, sequences, final_img_features, i, target = None):
         embedded = self.embedding(sequences)
         lstm_out, _ = self.lstm(embedded)
-        features = lstm_out[:, 0, :]
+        features = lstm_out[:, -1, :]
         features = self.fc1(features)
-        features = self.relu(features)  # Apply ReLU activation
-        features = self.dropout(features)  # Apply dropout
+        features = self.relu(features)
+        features = self.dropout(features)
         final_text_features = self.fc2(features)
-        final_text_features = self.relu(final_text_features)  # Apply ReLU activation
+        final_text_features = self.relu(final_text_features)
+
+        # print('IMAGE FFEATURES SHAPE : ', final_img_features.shape)
+        # print('TEXTUAL FEATURE SHAPES :', final_text_features.shape)
+
+        combined_features = torch.mul(final_text_features, final_img_features)
+        yhat, hidden_states = self.fc_combined(combined_features.unsqueeze(0))
+
+        yhat_probs = F.softmax(yhat, dim=2)
+        sequences = []
+        sequences.append(yhat_probs.squeeze(0))
+
+        for i in range(self.max_len-2):
+            yhat, hidden_states = self.fc_combined(hidden_states)
+            yhat_probs = F.softmax(yhat, dim=2)
+            sequences.append(yhat_probs.squeeze(0))
+            
+        return torch.cat([t.unsqueeze(1) for t in sequences], dim=1), final_img_features
+    
+    def get_word(self, sequences, final_img_features, i=-1, target = None):
+        embedded = self.embedding(sequences)
+        lstm_out, _ = self.lstm(embedded)
+        features = lstm_out[:, -1, :]
+        features = self.fc1(features)
+        features = self.relu(features)
+        features = self.dropout(features)
+        final_text_features = self.fc2(features)
+        final_text_features = self.relu(final_text_features)
 
         combined_features = torch.cat((final_text_features, final_img_features), dim=1)
         yhat, hidden_states = self.fc_combined(combined_features.unsqueeze(0))
 
         yhat_probs = F.softmax(yhat, dim=2)
-        _, yhat_index = torch.max(yhat_probs, dim=2)
 
-        return yhat_index, hidden_states
+        return yhat_probs, hidden_states
 
 class Discriminator(nn.Module):
-      def __init__(self, vocab_size, hidden_dim=64):
-          super(Discriminator, self).__init__()
-          self.embedding = nn.Embedding(vocab_size, hidden_dim)
-          self.rnn = nn.LSTM(hidden_dim, hidden_dim, batch_first=True)
-          self.fc = nn.Linear(hidden_dim, 1)
+    def __init__(self, image_feature_size = 256, embedding_dim = 300, hidden_size = 256, num_layers=1, dropout=0.5):
+        super(Discriminator, self).__init__()
+        
+        self.embedding = nn.Embedding(VOCAB_SIZE, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_size, num_layers=num_layers, batch_first=True, dropout=dropout)
+        
+        self.fc_intermediate = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(dropout)
+        )
+        
+        self.fc_final = nn.Linear(hidden_size, 1)
+        self.sigmoid = nn.Sigmoid()
 
-      def forward(self, x):
-          embedded = self.embedding(x)
-          _, (hidden, _) = self.rnn(embedded)
-          output = self.fc(hidden[-1, :, :])
-          return torch.sigmoid(output)
+    def forward(self, image_features, sequences, one_hot = True):
+        image_output = torch.relu(image_features)
+
+        if one_hot:
+            embedded_sequence = self.embedding(sequences.long())
+        else:
+            b=[]
+            for batch in range(sequences.shape[0]):
+                s = []
+                for seq in range(sequences.shape[1]):
+                    # print(seq.shape)
+                    s.append(torch.matmul(sequences[batch][seq],self.embedding.weight))
+                b.append(s)
+                embedded_sequence = torch.stack([torch.stack(inner_list) for inner_list in b], dim=0)
+        # print('EMBEDS SHAPE :', embedded_sequence.shape)
+        lstm_output, _ = self.lstm(embedded_sequence)
+        sequence_output = lstm_output[:, -1, :]  # Use only the last hidden state
+        
+        combined_features = torch.mul(image_output, sequence_output)
+        
+        intermediate_output = self.fc_intermediate(combined_features)
+        
+        output = self.fc_final(intermediate_output)
+        output = self.sigmoid(output)
+        return output
 
 class GAN(nn.Module):
     def __init__(self, generator, discriminator):
@@ -161,23 +190,26 @@ class ViTWithIntermediateOutput(ViTModel):
         return super().forward(pixel_values, return_dict=return_dict, output_hidden_states=output_hidden_states, **kwargs)
 
 model_with_intermediate = ViTWithIntermediateOutput.from_pretrained(model_name)
-model_with_intermediate.train()
+model_with_intermediate.eval()
 
 
 additional_layers = torch.nn.Sequential(
     torch.nn.Linear(197, 64),
     torch.nn.ReLU(),
-    torch.nn.Linear(64, 128)
+    torch.nn.Linear(64, 256)
 )
 
+# Freeze parameters of the ViT model
+for param in model_with_intermediate.parameters():
+    param.requires_grad = False
 
 def load_model(device):
-    model_with_intermediate.to(device)
-    gen=Generator(35,8485,model_with_intermediate,additional_layers,feature_extractor, device)
-    dis=Discriminator(8485)
-    gen.to(device)
-    dis.to(device)
-    gen.train()
-    dis.train()
+    model_with_intermediate #.to(device)
+    gen=Generator(35,8485,model_with_intermediate,feature_extractor, device)
+    dis=Discriminator()
+    # gen.to(device)
+    # dis.to(device)
+    # gen.train()
+    # dis.train()
 
     return gen, dis
--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from torch.nn.utils import clip_grad_norm_
 from torch import nn
 from tqdm import tqdm
 import warnings
+from torchviz import make_dot
 warnings.filterwarnings("ignore")
 from nltk.translate.bleu_score import corpus_bleu, SmoothingFunction
 # device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -23,6 +24,14 @@ from rouge_score import rouge_scorer
 import numpy as np
 scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
 
+torch.autograd.set_detect_anomaly(True)
+
+def set_gradients(model, grad = True):
+    for param in model.parameters():
+        print('Before :',param.grad)
+        print('Requires Grad :',param.requires_grad)
+        param.requires_grad = grad
+        print('After :', param.grad)
 
 def cal_rouge_scores(r_caption, f_caption):
     rouge1_p=[]
@@ -70,13 +79,17 @@ if os.path.exists(os.path.join(OUTPUT_DIR,'model.pkl')):
     gen=model['gen'].to(device)
     dis=model['dis'].to(device)
     start_epoch=model['epoch']
+
 else:
     print('INITIALIZING THE MODEL TRAINING')
     gen, dis = load_model(device)
-    gen= gen.to(device)
-    dis= dis.to(device)
+    # gen= gen.to(device)
+    # dis= dis.to(device)
     start_epoch = 0
 dataloader, tokenizer = load_data(DATA_FILE,IMAGE_DIR, BATCH_SIZE)
+gen= gen.to(device)
+# print(gen)
+dis= dis.to(device)
 word_index = tokenizer.index_word
 # print(word_index)
 
@@ -113,6 +126,17 @@ class WassersteinLoss(nn.Module):
 
         return wasserstein_distance
 # criterion = WassersteinLoss()
+    
+def batch_decode(model_output):
+    final_batch = []
+    for batch in model_output:
+        final_seq = []
+        for text in batch:
+            # print(text.shape)
+            _, yhat_index = torch.max(text, dim=0)
+            final_seq.append(yhat_index)
+        final_batch.append(final_seq)
+    return final_batch
 
 # g_loss = WassersteinLoss()
 g_loss = nn.BCELoss()
@@ -145,31 +169,49 @@ for epoch in range(EPOCH):
     for i, batch_items in enumerate(progbar):
         image_features = batch_items[0].to(device)
         real_captions = batch_items[1].to(device)
-        r_caption = decode(real_captions[0])
+        real_captions = real_captions[:, 1:]
 
-        fake_captions = gen(image_features, real_captions)
-        f_caption = decode(fake_captions[0])
-        
-        # with torch.no_grad():
-        pred_fake_labels = dis(fake_captions)
+        fake_captions, final_img_features = gen(image_features, real_captions)
+        fake_captions_tokenized = torch.argmax(fake_captions, dim=2)
+
+        # graph = make_dot(fake_captions, params=dict(gen.named_parameters()))
+        # graph.render("computation_graph_NEW")  # Save as PDF
+
+        # print(fake_captions)
+        r_caption = decode(real_captions[0])
+        f_caption = decode(fake_captions_tokenized[0])
+        # print(f_caption)
+        # print(r_caption)
+
+        pred_fake_labels = dis(final_img_features, fake_captions, one_hot=False)
 
         real_labels = torch.ones_like(pred_fake_labels).float()
         fake_labels = torch.zeros_like(pred_fake_labels).float()
 
-        generator_optimizer.zero_grad()
         generator_loss = g_loss(pred_fake_labels, real_labels)
         b_score = bleu_score(r_caption,f_caption)
         total_generator_loss=generator_loss
+        # set_gradients(gen)
+        generator_optimizer.zero_grad()
         generator_loss.backward()
-        clip_grad_norm_(gen.parameters(), max_norm=0.5)  # Adjust max_norm as needed
+        # clip_grad_norm_(gen.parameters(), max_norm=0.5)  # Adjust max_norm as needed
         generator_optimizer.step()
+        # count = 0
+        # for param in gen.parameters():
+        #     # print(param.grad)
+        #     if param.grad is not None:
+        #         count+=1
+        #         # print(f"Gradient AAA for {param}: {param.grad.norm()}")
 
-        pred_real_labels = dis(real_captions)
-        pred_fake_labels = dis(fake_captions.detach())
-        discriminator_optimizer.zero_grad()
+        # print(count)
+
+        # break
+        pred_real_labels = dis(final_img_features.detach(), real_captions)
+        pred_fake_labels = dis(final_img_features.detach(), fake_captions.detach(), one_hot = False)
         real_discriminator_loss = d_loss(pred_real_labels, real_labels)
         fake_discriminator_loss = d_loss(pred_fake_labels, fake_labels)
         total_discriminator_loss = real_discriminator_loss + fake_discriminator_loss
+        discriminator_optimizer.zero_grad()
         total_discriminator_loss.backward()
         discriminator_optimizer.step()
 
@@ -184,6 +226,7 @@ for epoch in range(EPOCH):
 
         progbar.set_description("EPOCH = %s/%s, G_LOSS = %0.3f, D_LOSS = %0.3f, current_loss = %0.3f, " %(epoch, EPOCH, np.mean(losses_G), np.mean(losses_D), total_generator_loss.item()))
 
+    # break 
     # rouge1_p, rouge1_r, rouge1_f, rouge2_p, rouge2_r, rouge2_f, rougel_p, rougel_r, rougel_f = cal_rouge_scores(r_captions, f_captions)
     # print(f'rouge1_precision : {rouge1_p}, rouge1_recall : {rouge1_r}, rouge2_f1 : {rouge2_f}, rouge2_precision : {rouge2_p}, rouge2_recall : {rouge2_r}, rouge2_f1 : {rougel_f}, rougel_precision : {rougel_p}, rougel_recall : {rougel_r}, rougel_f1 : {rougel_f},\n')
 
